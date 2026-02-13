279a17f400acbae6a7589ef86e5e9783d6bfaa94
Uğur Çalışkan <infougurcalskan@gmail.com>
2026-02-13 22:00:32 +0300

    feat: wire up app entry point with all sections

---
+++ src/App.tsx
@@ -0,0 +1,21 @@
+import StarField from "./components/StarField";
+import Navbar from "./components/Navbar";
+import Hero from "./components/Hero";
+import About from "./components/About";
+import Skills from "./components/Skills";
+import Projects from "./components/Projects";
+import Contact from "./components/Contact";
+
+export default function App() {
+  return (
+    <>
+      <StarField />
+      <Navbar />
+      <Hero />
+      <About />
+      <Skills />
+      <Projects />
+      <Contact />
+    </>
+  );
+}
